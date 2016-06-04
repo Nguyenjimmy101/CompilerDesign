ce8ab417cb571f283b1242b99e0cf44222ca3518
Nguyen <nguyenj11@wit.edu>
2016-06-04 18:58:58 -0400

    real

--- LexicalAnalyzer/Real.py
+++ LexicalAnalyzer/Real.py
@@ -1,2 +1,12 @@
+from LexicalAnalyzer.Tag import Tag
+from LexicalAnalyzer.Token import Token
+
+
 class Real(Token):
-    pass
+    value = 0
+
+    def __init__(self, v):
+        self.value = v
+
+    def __str__(self):
+        return 'REAL: Lexeme is %s and tag is %s' % (self.value, self.tag)
