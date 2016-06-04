1e51da7a2b46c82c2b2a611be33cbc536b0c1200
Nick Allevato <nicholas.allevato@gmail.com>
2016-06-04 15:23:53 -0400

    added float numbers

--- LexicalAnalyzer/Real.py
+++ LexicalAnalyzer/Real.py
@@ -1,2 +1,14 @@
+from LexicalAnalyzer.Token import Token
+from LexicalAnalyzer.Tag import Tag
+
+
 class Real(Token):
-    pass
+    value = None
+
+    def __init__(self, value):
+        super().__init__(Tag.REAL)
+
+        self.value = value
+
+    def __str__(self):
+        return 'REAL: value is %s and tag is %s' % (self.value, self.tag)
