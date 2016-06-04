923e206cf5d81b94561919420af6e998ca3df38f
Nick Allevato <nicholas.allevato@gmail.com>
2016-06-04 14:20:38 -0400

    Implement base of Word class

---
+++ LexicalAnalyzer/Word.py
@@ -0,0 +1,18 @@
+from .Token import Token
+from .Tag import Tag
+
+class Word(Token):
+    lexeme = ''
+
+    def __init__(self, s, tag):
+        super().__init__(tag)
+        self.lexeme = s
+
+    def __str__(self):
+        return 'WORD: Lexeme is %s and tag is %s' % (self.lexeme, self.tag)
+
+class Words(object):
+    eq = Word('==', Tag.EQ)
+    ne = Word('!=', Tag.NE)
+    le = Word('<=', Tag.LE)
+    ge = Word('>=', Tag.GE)
