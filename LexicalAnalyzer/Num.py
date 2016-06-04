aa7d1072837797a800032474d33166b00ac53931
Nick Allevato <nicholas.allevato@gmail.com>
2016-06-04 15:24:02 -0400

    number class

--- LexicalAnalyzer/Num.py
+++ LexicalAnalyzer/Num.py
@@ -1,10 +1,14 @@
 from LexicalAnalyzer.Token import Token
+from LexicalAnalyzer.Tag import Tag
 
 
 class Num(Token):
     value = None
 
-    def __init__(self, value, tag):
-        super().__init__(tag)
+    def __init__(self, value):
+        super().__init__(Tag.NUM)
 
         self.value = value
+
+    def __str__(self):
+        return 'NUM: value is %s and tag is %s' % (self.value, self.tag)
