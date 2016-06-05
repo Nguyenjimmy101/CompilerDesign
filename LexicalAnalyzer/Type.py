221366a6e35c28b4557b71d2279b07fe9ff6ccd4
Nick Allevato <nicholas.allevato@gmail.com>
2016-06-05 15:12:38 -0400

    Cleaned up and optimized the Type and Types classes

--- LexicalAnalyzer/Type.py
+++ LexicalAnalyzer/Type.py
@@ -1,49 +1,28 @@
-import null as null
-
-from LexicalAnalyzer.Token import Token
-from LexicalAnalyzer.Word import Word, Words
 from LexicalAnalyzer.Tag import Tag
+from LexicalAnalyzer.Token import Token
+from LexicalAnalyzer.Word import Word
 
 
 def numeric(p):
-    return p == Type.Char or p == Type.Int or p == Type.Float
-
-
-def __lt__(self, other):
-    return self < other
-
-
-def __gt__(self, other):
-    return self > other
-
-
-def max(p1, p2):
-    if not (numeric(p1)) or not (numeric(p2)):
-        return null
-    elif __eq__(p1, p2, Type.Float):
-        return Type.Float
-    elif __eq__(p1, p2, Type.Int):
-        return Type.Int
-    else:
-        return Type.Char
+    return p in (Types.INT, Types.FLOAT)
 
 
 class Type(Token):
     width = 0
 
-    def __init__(self, w, tag):
+    def __init__(self, width, tag):
         super().__init__(tag)
-        self.width = w
+        self.width = width
 
     def __str__(self):
-        return 'TYPE: Lexeme is %s tag is %s Width is %s' % (self.lexeme, self.tag, self.width)
+        return 'TYPE: Lexeme is %s tag is %s width is %s' % (self.lexeme, self.tag, self.width)
 
     def __eq__(self, p, types):
         return self == types or p == types
 
 
-class Type(object):
-    Int = Word('int', Tag.BASIC, 4)
-    Float = Word('float', Tag.BASIC, 8)
-    Char = Word('char', Tag.BASIC, 1)
-    Bool = Word('bool', Tag.BASIC, 1)
+class Types(object):
+    INT = Word('int', Tag.BASIC)
+    FLOAT = Word('float', Tag.BASIC)
+    STRING = Word('string', Tag.BASIC)
+    BOOL = Word('bool', Tag.BASIC)
