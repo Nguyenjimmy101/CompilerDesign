2451b000082f565943e452b0efcd4d1f82ec443b
Nick Allevato <nicholas.allevato@gmail.com>
2016-06-06 19:04:38 -0400

    Fix REAL to NUM for numbers

--- LexicalAnalyzer/Num.py
+++ LexicalAnalyzer/Num.py
@@ -5,7 +5,7 @@ from LexicalAnalyzer.Tag import Tag
 class Num(BaseNumber):
 
     def __init__(self, value):
-        super().__init__(value, Tag.REAL)
+        super().__init__(value, Tag.NUM)
 
     def __str__(self):
         return 'NUM: value is %s and tag is %s' % (self.value, self.tag)
