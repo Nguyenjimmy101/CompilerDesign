89e563b749c1166448f68dcc6e8c9f3dc9c4eb58
Nguyen <nguyenj11@wit.edu>
2016-06-13 11:26:54 -0400

    Change the lexer main a bit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from LexicalAnalyzer.Lexer import Lexer
 if __name__ == '__main__':
     lexer = Lexer()
     while True:
-        out = lexer.scan
+        out = lexer.scan()
         if isinstance(out, bool) and out == False:
             print('END')
             break
