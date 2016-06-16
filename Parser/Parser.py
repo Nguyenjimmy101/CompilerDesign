73fa3ee656f616e4afd8bc9eddb5b6428dcad4d4
Nick Allevato <nicholas.allevato@gmail.com>
2016-06-16 14:10:35 -0400

    Update to the parser

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,5 +1,6 @@
 from LexicalAnalyzer import Token
 from LexicalAnalyzer.Lexer import Lexer
+from LexicalAnalyzer.Tag import Tag
 
 
 class Parser(object):
@@ -10,19 +11,47 @@ class Parser(object):
 
     def __init__(self, stream):
         self.lex = Lexer(stream)
+        self.move()
 
     def move(self):
         self.look = self.lex.scan()
-
-    def error(self, s):
-        print("Near Line " + self.lex.line + ": " + s)
 
     def match(self, tag):
         if self.look.tag == tag:
             self.move()
         else:
-            raise SyntaxError('Expected %s, got %s' % (tag, self.look.tag))
+            raise SyntaxError('Expected %s, got %s' % (tag, self.look))
 
     def block(self):
         #statements
         pass
+
+    def relop(self):
+        if self.look.tag == Tag.LT:
+            self.match(Tag.LT)
+        elif self.look.tag == Tag.GT:
+            self.match(Tag.GT)
+        elif self.look.tag == Tag.GE:
+            self.match(Tag.GE)
+        elif self.look.tag == Tag.LE:
+            self.match(Tag.LE)
+        elif self.look.tag == Tag.EQ:
+            self.match(Tag.EQ)
+        elif self.look.tag == Tag.NE:
+            self.match(Tag.NE)
+
+    def expr(self):
+        pass
+
+    def stmt(self):
+        if self.look.tag == Tag.IF:
+            self.match(Tag.IF)
+            self.relop()
+            self.expr()
+            self.expr()
+            self.block()
+
+    def assign(self):
+        self.match(Tag.ASSIGN)
+        self.match(Tag.ID)
+        self.expr()
