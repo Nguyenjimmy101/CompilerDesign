35fd887f4052090fbc1c5ce0f6ae36b151cd14d9
Nick Allevato <nicholas.allevato@gmail.com>
2016-06-16 13:20:39 -0400

    Fixed up beginning of parser, added a new sample file for testing, changed main file to use the parser

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,28 +1,28 @@
-import null
-
 from LexicalAnalyzer import Token
 from LexicalAnalyzer.Lexer import Lexer
 
 
-class Parser:
-    lex = Lexer()
-    look = Token
-    top = null
+class Parser(object):
+    lex = None
+    look = None
+    top = None
     used = 0
+
+    def __init__(self, stream):
+        self.lex = Lexer(stream)
 
     def move(self):
         self.look = self.lex.scan()
 
     def error(self, s):
-        print("Near Line " + self.Lexer.line + ": " + s)
+        print("Near Line " + self.lex.line + ": " + s)
 
-    def match(self, t):
-        if self.look.tag == t:
+    def match(self, tag):
+        if self.look.tag == tag:
             self.move()
         else:
-            self.error("Syntax Error")
+            raise SyntaxError('Expected %s, got %s' % (tag, self.look.tag))
 
-
-    #def block(self):
-    #statements
-
+    def block(self):
+        #statements
+        pass
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sys
 
-from LexicalAnalyzer.Lexer import Lexer
+from Parser.Parser import Parser
 
-def run_lexer(lexer):
+def run_parser(parser):
     while True:
-        out = lexer.scan()
-        if isinstance(out, bool) and out == False:
-            print('END')
+        parser.move()
+        if isinstance(parser.look, bool) and parser.look == False:
             break
-        print(out)
+        print(parser.look)
+    print('END ALL')
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         with open(sys.argv[1]) as fp:
-            run_lexer(Lexer(fp))
+            run_parser(Parser(fp))
     else:
-        run_lexer(Lexer(sys.stdin))
+        run_parser(Parser(sys.stdin))
