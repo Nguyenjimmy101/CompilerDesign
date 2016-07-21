8b679ad08614871ce2b520ab07830e769bf68a3a
Nguyen <nguyenj11@wit.edu>
2016-07-20 23:35:41 -0400

    Worked on the node.py

--- Inter/Node.py
+++ Inter/Node.py
@@ -1,8 +1,19 @@
 from LexicalAnalyzer.Lexer import Lexer
 
+
 class Node(object):
-    # lexline = 0
+    lexline = 0
 
     def __init__(self):
-        pass
-        # self.lexline = Lexer.line
+        self.lexline = Lexer.line
+
+    def error(self, s):
+        raise SyntaxError('near line %s, : %s' % (self.lexline, s))
+
+    labels = 0
+
+    def newlabel(self):
+        return self.labels
+
+    def emitlabel(self, i):
+        print("L %s :" % i)
