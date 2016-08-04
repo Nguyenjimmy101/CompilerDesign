a87ed8dbd0fca4ee9015b8af003ad48d3967db83
Nick Allevato <nicholas.allevato@gmail.com>
2016-08-04 12:29:54 -0400

    Removed commented code blocks

--- Inter/Elif.py
+++ Inter/Elif.py
@@ -5,9 +5,6 @@ class Elif(Stmt):
     def __init__(self, rel, block):
         super().__init__()
         self.rel = rel
-        # self.operator = operator
-        # self.expr1 = expr1
-        # self.expr2 = expr2
         self.block = block
         self.type = 'Elif'
 
--- Inter/If.py
+++ Inter/If.py
@@ -6,9 +6,6 @@ class If(Stmt):
     def __init__(self, rel, block):
         super().__init__()
         self.rel = rel
-        # self.operator = operator
-        # self.expr1 = expr1
-        # self.expr2 = expr2
         self.block = block
         self.type = 'If'
 
